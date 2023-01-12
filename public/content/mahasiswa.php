f31c4a369b26b6e61e236399fb3979578159bd1b
Dheni Subenk <dsubenk@192.168.1.7>
2023-01-12 10:12:51 +0900

    crud 2

---
+++ public/content/mahasiswa.php
@@ -0,0 +1,67 @@
+<div class="row">
+    <div class="col-12">
+        <h2>Mahasiswa</h2>
+        <div class="card">
+            <div class="card-body">
+                <form action="index.php?page=mahasiswa_save" method="POST">
+                    <div class="mb-2 row">
+                        <div class="col-6">
+                            <label for="" class="form-label">NIM</label>
+                            <input type="text" name="nim" class="form-control">
+                        </div>
+                        <div class="col-6">
+                            <label for="" class="form-label">Nama</label>
+                            <input type="text" name="nama" class="form-control">
+                        </div>
+                    </div>
+                    <div class="mb-2">
+                        <label for="" class="form-label">Jurusan</label>
+                        <select name="jurusan" class="form-select" id="">
+                            <option>Teknik Informatika</option>
+                            <option>Sistem Informasi</option>
+                            <option>Manajemen Informatika</option>
+                        </select>
+                    </div>
+                    <div class="mb-2">
+                        <label for="" class="form-label">Alamat</label>
+                        <input type="text" name="alamat" class="form-control">
+                    </div>
+                    <div class="mb-2">
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="card mt-2">
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>NIM</th>
+                            <th>NAMA</th>
+                            <th>JURUSAN</th>
+                            <th>ALAMAT</th>
+                            <th>#</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql = $con->query("SELECT * FROM mahasiswa");
+                        while ($row = $sql->fetch()) {
+                            echo "<tr>
+                                    <td>$row[nim]</td>
+                                    <td>$row[nama]</td>
+                                    <td>$row[jurusan]</td>
+                                    <td>$row[alamat]</td>
+                                    <td></td>
+                                </tr>";
+                        }
+                        ?>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ public/index.php
@@ -0,0 +1,68 @@
+<?php
+require_once '../config/fungsi.php';
+require_once 'config/koneksi.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: aliceblue;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container-fluid p-0">
+        <div class="row">
+            <div class="col-12">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div class="container">
+                        <a class="navbar-brand" href="index.php">web2<strong>ti3f</strong> </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div class="navbar-nav">
+                                <a class="nav-link" href="index.php">Home</a>
+                                <a class="nav-link" href="index.php?page=mahasiswa">Mahasiswa</a>
+                                <a class="nav-link" href="index.php?page=galeri">Galeri</a>
+                                <a class="nav-link" href="index.php?page=kontak">Kontak</a>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-3">
+        <!-- konten -->
+        <?php
+        $dir = "content";
+        $page = @$_GET['page'];
+        if (empty($page)) {
+            include 'content/home.php';
+        } else {
+            $scan = scanFile($dir, $page);
+            if ($scan === 1) {
+                include "content/$page.php";
+            } else {
+                include "content/404.php";
+            }
+        }
+        ?>
+        <!-- konten -->
+    </div>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
